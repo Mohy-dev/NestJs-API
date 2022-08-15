b2aa4f84c46837ffa46bbae76c9bb0b23dc31f5e
mohy-dev <mohy.dev@gmail.com>
2022-08-15 05:41:27 +0200

    status codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,23 @@
 /* eslint-disable */
 import { AuthService } from './auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+} from '@nestjs/common';
 import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
-  @Post('signup')
+
+  @Post('signup') // 201 Created by default
   signUp(@Body() dto: AuthDto) {
     return this.authService.signUp(dto);
   }
+
+  @HttpCode(200) // Or @Httpcode(HttpStatus.OK) => const enums btw
   @Post('signin')
   signIn(@Body() dto: AuthDto) {
     return this.authService.signIn(dto);
